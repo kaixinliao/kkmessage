f3708e8398c2e712b0ce5adb435a75254d8d2b67
kaixinliao <134143875+kaixinliao@users.noreply.github.com>
2026-05-20 05:15:55 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,7 +4,7 @@ import json
 from requests.exceptions import RequestException
 
 # ===================== 配置 =====================
-API_URL = "http://konk.cc/customer/account/unread_conversation_list?token=vV34pGy73k8hzEgr5B9pJLU43CHt7LOx&limit=50"
+API_URL = "http://konk.cc/customer/account/unread_conversation_list?token=7P8h36JcK8pRrBuSDIRymrOV7pHmZrnV&limit=50"
 BARK_API_KEY = "xAeazNMHEvvUCEq3LsZyqa"
 INTERVAL = 20
 last_message_ids = {}
